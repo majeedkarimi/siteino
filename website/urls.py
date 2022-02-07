2f84d3524c25f5520397a2f4c2904c17c24e828c
majeedkarimi <46189036+majeedkarimi@users.noreply.github.com>
2022-02-07 19:59:59 +0330

    create base.html
    + base.html (extends)

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from website.views import index_view,about_view,contact_view
 urlpatterns = [
-    path('',index_view),
-    path('about',about_view),
-    path('contact',contact_view),
+    path('', index_view),
+    path('about', about_view),
+    path('contact', contact_view),
 ]
