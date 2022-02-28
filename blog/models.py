e43ab0aff6f748f2e424255b5b991b33beb7fd01
majid <abm.karimi@gmail.com>
2022-02-28 17:11:05 +0330

    register models in admin
    + registering modelsin admin
    + add __str__ in models

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,6 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     cretaed_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"({self.id}) {self.title}"
