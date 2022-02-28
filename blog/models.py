6d22c72927d065f41438a25b057f6f96f67c02d6
majid <abm.karimi@gmail.com>
2022-02-28 11:27:32 +0330

    create models
    + modes blog
    + models contact

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=300)
     content = models.TextField()
+    # category
+    # tag
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    cretaed_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
