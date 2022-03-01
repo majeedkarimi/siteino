f84f767bd7e805fb404ce6c7d1311d15149e941e
majid <abm.karimi@gmail.com>
2022-03-01 11:39:30 +0330

    adding Contact to admin
    add Contact model in admin
    and
    + Meta
    + class ContactAdmin
    + __str__ in models,py

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    # ordering = ['-created_date']
+    list_filter = ['email']
+    empty_value_display = '-empty-'
+    list_display = ['name','email','created_date']
+    search_fields = ['message','subject']
+admin.site.register(Contact,ContactAdmin)
