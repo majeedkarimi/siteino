ec4a71a2f6268ffcffab90097084b948b3476e67
majid <abm.karimi@gmail.com>
2022-03-01 17:56:55 +0330

    show data from views
    create query
    send query to template

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 
 def index_blog(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    content = {'posts':posts}
+    return render(request, 'blog/blog-home.html',content)
 
 
 def single_blog(request):
