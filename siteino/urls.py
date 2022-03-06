7efbad4ab94eb531b556ea0b43803df1a426c454
majid <abm.karimi@gmail.com>
2022-03-06 23:47:12 +0330

    create image field
    create image field in post.models
    and
    edit urls siteino

--- siteino/urls.py
+++ siteino/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from distutils.debug import DEBUG
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -26,6 +26,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
