a17adadadd1b43c0276f37974eb5645e514bd28e
majid <abm.karimi@gmail.com>
2022-03-07 22:06:21 +0330

    create manaytomany category field
    creating category manytomany field

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title','content','counted_views','status')
-    list_display = ('author','pk','title','status','created_date',
+    list_display = ('title','author','pk','status','created_date',
                     'updated_date','published_date')
     list_filter = ('status','author')
     search_fields = ('title','content',)
     # ordering = ["-created_date"]
     
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=300)
     content = models.TextField()
-    # category
+    category = models.ManyToManyField(Category)
     # tag
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
@@ -19,3 +26,4 @@ class Post(models.Model):
     
     class Meta:
         ordering =['-created_date']
+
