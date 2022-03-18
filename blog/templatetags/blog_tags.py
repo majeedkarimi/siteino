8e1a58e0d10429c522bb4b57a6cf82cda4acb95e
majid <abm.karimi@gmail.com>
2022-03-18 18:12:49 +0330

    post categories with inclusion_tag
    post categories with inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -2,9 +2,15 @@ from django import template
 
 register = template.Library()
 
-from blog.models import Post
+from blog.models import Post,Category
+from django.db.models import Count
 
 @register.inclusion_tag('blog/blog-latest-posts.html')
 def latest_posts(args):
     posts=Post.objects.filter(status=1).order_by('-published_date')[:args]
     return {"posts":posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    categories = Category.objects.filter(post__status=1).annotate(Count("post"))
+    return {"categories":categories}
