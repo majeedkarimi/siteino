a0dca08b89e0cde5e19cfd475ef63a691003b8fe
majid <abm.karimi@gmail.com>
2022-03-26 21:11:13 +0430

    django summernote
    create django summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from blog.models import Post,Category
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title','content','counted_views','status')
@@ -11,6 +12,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status','author')
     search_fields = ('title','content',)
     # ordering = ["-created_date"]
+    summernote_fields = ('content',)
     
 admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
--- siteino/settings.py
+++ siteino/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'robots',
     "debug_toolbar",
     "taggit",
+    'django_summernote',
 ]
 
 # sites framework
@@ -56,6 +57,56 @@ SITE_ID = 2
 ROBOTS_USE_HOST = True
 ROBOTS_USE_SITEMAP = True
 
+# summernote configs
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+SUMMERNOTE_THEME = 'bs4'
+
+SUMMERNOTE_CONFIG = {
+    # Using SummernoteWidget - iframe mode, default
+    'iframe': True,
+
+    # You can put custom Summernote settings
+    'summernote': {
+        # As an example, using Summernote Air-mode
+        'airMode': False,
+
+        # Change editor size
+        'width': '100%',
+        'height': '480',
+
+        # Use proper language setting automatically (default)
+        'lang': None,
+
+        # Toolbar customization
+        # https://summernote.org/deep-dive/#custom-toolbar-popover
+        'toolbar': [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['fullscreen', 'codeview', 'help']],
+        ],
+
+        # Or, explicitly set language/locale for editor
+        'lang': 'ko-KR',
+
+        # You can also add custom settings for external plugins
+        'print': {
+            'stylesheetUrl': '/some_static_folder/printable.css',
+        },
+        'codemirror': {
+            'mode': 'htmlmixed',
+            'lineNumbers': 'true',
+            # You have to include theme file in 'css' or 'css_for_inplace' before using it.
+            'theme': 'monokai',
+        },
+    }
+}
+
+    
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
